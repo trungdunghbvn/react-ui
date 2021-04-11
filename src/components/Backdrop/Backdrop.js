3326498ac9abbdffea88f454a68b19567e5dbbe2
trungdunghbvn@gmail.com <trungdunghbvn@gmail.com>
2021-04-11 20:39:25 +0700

    update modal

---
+++ src/components/Backdrop/Backdrop.js
@@ -0,0 +1,6 @@
+import React from "react";
+import styles from "./Backdrop.scss";
+
+export function Backdrop() {
+  return <div className={styles.Backdrop}/>;
+}
--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,5 @@
 export {Heading} from './Heading';
 export {OnOff} from './OnOff';
 export {CrossSell} from './CrossSell';
+export {Modal} from './Modal';
+export {Backdrop} from './Backdrop';
